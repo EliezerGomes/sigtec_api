926c6e9c80adfa05b628edc18176fd975aa45465
EliezerGomes <eliezerportilho@gmail.com>
2023-03-22 10:42:26 -0300

    versao beta

--- src/revenda/listarRevendaService.ts
+++
@@ -1,9 +0,0 @@
-import { prismaClient } from "../../database/prismaClient";
-
-export class ListarRevendaService {
-    async execute() {
-        const revenda = await prismaClient.revenda.findMany()
-
-        return revenda
-    }
-}
--- src/terminais/listarTerminais.ts
+++
@@ -1,9 +0,0 @@
-import { prismaClient } from "../../database/prismaClient";
-
-export class ListarTermianis {
-    async execute() {
-        const terminais = await prismaClient.terminais.findMany()
-
-        return terminais
-    }
-}
